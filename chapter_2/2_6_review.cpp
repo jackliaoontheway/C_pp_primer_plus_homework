cc05b93858e10390669ceabe0353ebfdfdee4306
Jack <ldhontheway@live.com>
2020-01-02 14:37:08 +0800

    Finished review added work

--- chapter_2/2_6_review.cpp
+++ chapter_2/2_6_review.cpp
@@ -2,21 +2,31 @@
 
 
 
-
 int main() {
 
+    //1. C++ 程序的模块叫什么 ? 函数
+    //2. #include <iostream> 引入iostream库,在当前文件引入iostream文件
+    //3. using namespace std 使用C++标准库 std,程序可以直接使用std的函数或变量而不需要使用std::
+            //namespace是命名空间 用于标识库，防止和第三方的库冲突
+    //4. 
+        std::cout << "Hello world" << std::endl;
+    //5. 
+        int chesses;
+    //6.
+        chesses = 32;
+    //7.
+        std::cin >> chesses;
+    //8.
+        std::cout << "We have" + std::to_string(chesses) + "of chesse";
+    //9. 
+        //int froop(double t); 返回值是int 函数名 参数是double类型
+        //void rattle(int n); 没有返回值 函数名 rattle 参数是int类型
+        //int prune(void); 返回值是int 函数名 参数是空
+    //10. 函数定义需要返回值
+    //11. main方法外 using namespace std; std::cout; main方法内使用 using namespace std;
+
     using namespace std;
+    std::cout << "Jack Liao" << endl;
+
     
-    std::cout << "Hello World" << endl;
-
-    int cheeses;
-    cheeses = 32;
-    //cin >> cheeses;
-
-    string s = "123";
-    // to_string() int to string
-    // s.c_str() 
-    // atoi(s.c_str()) string to int
-    cout << atoi(s.c_str()) + 1 << endl;
-    std::cout << "We have " + to_string(cheeses) + " ovarieties of cheese" << endl;
 }
